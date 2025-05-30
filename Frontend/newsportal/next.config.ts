b3b31426f127d2e73ff1626b4b96a5e901ff224d
sudhir4500 <sudhirsharma4500@gmail.com>
2025-05-30 10:56:27 +0545

    next config

--- Frontend/newsportal/next.config.ts
+++ Frontend/newsportal/next.config.ts
@@ -6,6 +6,7 @@ const nextConfig = {
       {
         protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL || 'http', // Default to 'http' for development
         hostname: process.env.NEXT_PUBLIC_API_HOST || '127.0.0.1', // Default to localhost for development
+        port:"", // No port specified
         pathname: '/media/**', // Allow all paths under /media/
       },
     ],
