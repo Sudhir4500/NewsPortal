7dbf171dc8f81997a9deadbed7755d41b4ca9234
sudhir4500 <sudhirsharma4500@gmail.com>
2025-06-03 02:16:30 +0545

    temp remove the timeout of api

--- Frontend/newsportal/src/app/api/api.ts
+++ Frontend/newsportal/src/app/api/api.ts
@@ -4,7 +4,8 @@ import { getToken } from './cookie';
 
 const api: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/',
-  timeout: 20000, // 20 seconds timeout for requests means we won't wait indefinitely
+  // timeout: 20000,
+  //  20 seconds timeout for requests means we won't wait indefinitely
   // DO NOT set 'Content-Type' here — let Axios set it automatically per request
 });
 
