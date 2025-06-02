b144e435397bc374c20ed6bf8bd83aa51620c9bf
sudhir4500 <sudhirsharma4500@gmail.com>
2025-06-02 21:45:08 +0545

    enchance api call'

--- Frontend/newsportal/next.config.ts
+++ Frontend/newsportal/next.config.ts
@@ -2,20 +2,20 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //     protocol: 'https',
-      //     // Allow HTTPS protocol
-      //     hostname: 'res.cloudinary.com',
-      //     port: '',
-      //     pathname: '/**', // Allow all paths under res.cloudinary.com
-      //   },
-        {
-          protocol: 'http',
-          // Allow HTTP protocol
-          hostname: '127.0.0.1',
-          port: '8000', // Allow port 8000 for local development
-          pathname: '/media/**', // Allow all paths under 127.0.0.1
-      }
+      {
+          protocol: 'https',
+          // Allow HTTPS protocol
+          hostname: 'res.cloudinary.com',
+          port: '',
+          pathname: '/**', // Allow all paths under res.cloudinary.com
+        }
+      //   {
+      //     protocol: 'http',
+      //     // Allow HTTP protocol
+      //     hostname: '127.0.0.1',
+      //     port: '8000', // Allow port 8000 for local development
+      //     pathname: '/media/**', // Allow all paths under 127.0.0.1
+      // }
     ],
   },
 };
--- backend/news/models.py
+++ backend/news/models.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 from taggit.managers import TaggableManager
 from .tagging import CustomUUIDTaggedItem
 
-# from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -37,8 +37,8 @@ class News(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     tags = TaggableManager(through=CustomUUIDTaggedItem, blank=True)
-    image = models.ImageField(upload_to='news/', blank=True, null=True)
-    # image=CloudinaryField('image', blank=True, null=True, help_text="Upload an image for the news article. Supported formats: jpg, png, webp.")
+    # image = models.ImageField(upload_to='news/', blank=True, null=True)
+    image=CloudinaryField('image', blank=True, null=True, help_text="Upload an image for the news article. Supported formats: jpg, png, webp.")
     published_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_trending = models.BooleanField(default=False, help_text="Check to mark this news as trending.")
