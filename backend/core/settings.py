7855fdbb5dcbd75ecb5bcda17a33493e7529c837
sudhir4500 <sudhirsharma4500@gmail.com>
2025-06-04 11:49:36 +0545

    dajfjla

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
