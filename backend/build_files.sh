0746ebb393e5e7c7b7bc62df335a9cd7368a615b
sudhir4500 <sudhirsharma4500@gmail.com>
2025-06-04 09:58:14 +0545

    vercel file updated

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -5,11 +5,11 @@ set -e
 pip3 install -r requirements.txt
 
 # Run migrations (optional, only if needed in Vercel)
-python3 manage.py makemigrations
-python3 manage.py migrate 
+python manage.py makemigrations
+python manage.py migrate
 
 # Collect static files to a directory Vercel can serve
-python3 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 # Copy static files to the output directory for Vercel
 mkdir -p .vercel/output/static
