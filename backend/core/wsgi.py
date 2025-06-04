9f10f2904b77b8a6c062b84699dd9e9607ac8caa
sudhir4500 <sudhirsharma4500@gmail.com>
2025-06-04 10:41:18 +0545

    testing

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from serverless_wsgi import handle_request
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
@@ -17,3 +18,6 @@ application = get_wsgi_application()
 # This WSGI configuration is used to serve the Django application in production.
 app = application  # For compatibility with some WSGI servers that expect 'app' to be the callable.
 
+
+def handler(event, context):
+    return handle_request(application, event, context)
